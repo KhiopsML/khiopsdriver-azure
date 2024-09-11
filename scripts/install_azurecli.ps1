8d02a48c6213e13c7b206af7f868b74303bf7ecd
Stephane Gouache <stephane.gouache@orange.com>
2024-09-11 11:05:15 +0200

    Try fix windows install

---
+++ scripts/install_azurecli.ps1
@@ -0,0 +1,6 @@
+#!/usr/bin/env powershell
+
+$ProgressPreference = 'SilentlyContinue'; 
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; 
+Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; 
+Remove-Item .\AzureCLI.msi
