4fd462131ab4bb56b7209839ee0fd1b2a0b150f1
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2025-06-11 11:55:31 +0200

    Add basic_test.h to hold constants and split test suite in two parts

--- test/basic_test.cpp
+++ test/basic_test.cpp
@@ -1,7 +1,7 @@
+#include "basic_test.h"
 #include "azureplugin.h"
 #include "azureplugin_internal.h"
 
-#include <array>
 #include <cstring>
 #include <functional>
 #include <iostream>
@@ -21,29 +21,27 @@
 
 using namespace azureplugin;
 
-//using ::testing::Return;
-
-TEST(AzureDriverTest, GetDriverName)
+TEST(BasicTest, GetDriverName)
 {
     ASSERT_STREQ(driver_getDriverName(), "Azure driver");
 }
 
-TEST(AzureDriverTest, GetVersion)
+TEST(BasicTest, GetVersion)
 {
     ASSERT_STREQ(driver_getVersion(), DRIVER_VERSION);
 }
 
-TEST(AzureDriverTest, GetScheme)
+TEST(BasicTest, GetScheme)
 {
     ASSERT_STREQ(driver_getScheme(), "https");
 }
 
-TEST(AzureDriverTest, IsReadOnly)
+TEST(BasicTest, IsReadOnly)
 {
     ASSERT_EQ(driver_isReadOnly(), kFalse);
 }
 
-TEST(AzureDriverTest, Connect)
+TEST(BasicTest, Connect)
 {
     //check connection state before call to connect
     ASSERT_EQ(driver_isConnected(), kFalse);
@@ -57,21 +55,21 @@ TEST(AzureDriverTest, Connect)
     ASSERT_EQ(driver_isConnected(), kFalse);
 }
 
-TEST(AzureDriverTest, Disconnect)
+TEST(BasicTest, Disconnect)
 {
     ASSERT_EQ(driver_connect(), kSuccess);
     ASSERT_EQ(driver_disconnect(), kSuccess);
     ASSERT_EQ(driver_isConnected(), kFalse);
 }
 
-TEST(AzureDriverTest, GetFileSize)
+TEST(BasicTest, GetFileSize)
 {
 	ASSERT_EQ(driver_connect(), kSuccess);
 	ASSERT_EQ(driver_getFileSize(test_single_file), 5585568);
 	ASSERT_EQ(driver_disconnect(), kSuccess);
 }
 
-TEST(AzureDriverTest, GetMultipartFileSize)
+TEST(BasicTest, GetMultipartFileSize)
 {
 	ASSERT_EQ(driver_connect(), kSuccess);
     /// TODO: Replace URL below with actual Azure cloud storage URL
@@ -79,7 +77,7 @@ TEST(AzureDriverTest, GetMultipartFileSize)
 	ASSERT_EQ(driver_disconnect(), kSuccess);
 }
 
-TEST(AzureDriverTest, GetFileSizeNonexistentFailure)
+TEST(BasicTest, GetFileSizeNonexistentFailure)
 {
 	ASSERT_EQ(driver_connect(), kSuccess);
     /// TODO: Replace URL below with actual Azure cloud storage URL
@@ -88,7 +86,7 @@ TEST(AzureDriverTest, GetFileSizeNonexistentFailure)
 	ASSERT_EQ(driver_disconnect(), kSuccess);
 }
 
-TEST(AzureDriverTest, FileExists)
+TEST(BasicTest, FileExists)
 {
 	ASSERT_EQ(driver_connect(), kSuccess);
     /// TODO: Replace URL below with actual Azure cloud storage URL
@@ -96,7 +94,7 @@ TEST(AzureDriverTest, FileExists)
 	ASSERT_EQ(driver_disconnect(), kSuccess);
 }
 
-TEST(AzureDriverTest, DirExists)
+TEST(BasicTest, DirExists)
 {
 	ASSERT_EQ(driver_connect(), kSuccess);
     /// TODO: Replace URL below with actual Azure cloud storage URL
@@ -106,31 +104,26 @@ TEST(AzureDriverTest, DirExists)
 
 #ifndef _WIN32
 // Setting of environment variables does not work on Windows
-TEST(AzureDriverTest, DriverConnectMissingCredentialsFailure)
+TEST(BasicTest, DriverConnectMissingCredentialsFailure)
 {
     FAIL(); // To be implemented
 }
 
 void setup_bad_credentials() {
-    std::stringstream tempCredsFile;
-#ifdef _WIN32
-	tempCredsFile << std::getenv("TEMP") << "\\creds-" << boost::uuids::random_generator()() << ".json";
-#else
-	tempCredsFile << "/tmp/creds-" << boost::uuids::random_generator()() << ".json";
-#endif
-    std::ofstream outfile (tempCredsFile.str());
-    outfile << "{}" << std::endl;
-    outfile.close();
     auto env = boost::this_process::environment();
-    env["GCP_TOKEN"] = tempCredsFile.str();
+    env["AZURE_STORAGE_CONNECTION_STRING"] =
+        // Default Azurite credentials with AccountKey component slightly modified (last "w" replaced by "W")
+        "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey="
+        "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/"
+        "KBHBeksoGMGW==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;";
 }
 
 void cleanup_bad_credentials() {
     auto env = boost::this_process::environment();
-    env.erase("GCP_TOKEN");
+    env.erase("AZURE_STORAGE_CONNECTION_STRING");
 }
 
-TEST(AzureDriverTest, GetFileSizeInvalidCredentialsFailure)
+TEST(BasicTest, GetFileSizeInvalidCredentialsFailure)
 {
     setup_bad_credentials();
 	ASSERT_EQ(driver_connect(), kSuccess);
@@ -141,38 +134,21 @@ TEST(AzureDriverTest, GetFileSizeInvalidCredentialsFailure)
 }
 #endif
 
-TEST(AzureDriverTest, RmDir)
+TEST(BasicTest, RmDir)
 {
     ASSERT_EQ(driver_connect(), kSuccess);
 	ASSERT_EQ(driver_rmdir("dummy"), kSuccess);
 	ASSERT_EQ(driver_disconnect(), kSuccess);
 }
 
-TEST(AzureDriverTest, MkDir)
+TEST(BasicTest, MkDir)
 {
 	ASSERT_EQ(driver_connect(), kSuccess);
 	ASSERT_EQ(driver_mkdir("dummy"), kSuccess);
 	ASSERT_EQ(driver_disconnect(), kSuccess);
 }
 
-TEST(AzureDriverTest, GetSystemPreferredBufferSize)
+TEST(BasicTest, GetSystemPreferredBufferSize)
 {
 	ASSERT_EQ(driver_getSystemPreferredBufferSize(), 4 * 1024 * 1024);
 }
-
-constexpr const char* test_dir_name = "http://127.0.0.1:10000/devstoreaccount1/data-test-khiops-driver-azure/khiops_data/bq_export/Adult/";
-
-constexpr const char* test_non_existent_file = "http://127.0.0.1:10000/devstoreaccount1/data-test-khiops-driver-azure/khiops_data/samples/non_existent_file.txt";
-// Actual Azure storage URI: https://myaccount.file.core.windows.net/myshare/myfolder/myfile.txt
-constexpr const char* test_single_file = "http://127.0.0.1:10000/devstoreaccount1/data-test-khiops-driver-azure/khiops_data/samples/Adult/Adult.txt";
-constexpr const char* test_glob_file = "http://127.0.0.1:10000/devstoreaccount1/data-test-khiops-driver-azure/khiops_data/bq_export/Adult/Adult-split-00000000000*.txt";
-constexpr const char* test_range_file_one_header = "http://127.0.0.1:10000/devstoreaccount1/data-test-khiops-driver-azure/khiops_data/split/Adult/Adult-split-0[0-5].txt";
-constexpr const char* test_glob_file_header_each = "http://127.0.0.1:10000/devstoreaccount1/data-test-khiops-driver-azure/khiops_data/bq_export/Adult/*.txt";
-constexpr const char* test_double_glob_header_each = "http://127.0.0.1:10000/devstoreaccount1/data-test-khiops-driver-azure/khiops_data/split/Adult_subsplit/**/Adult-split-*.txt";
-
-constexpr std::array<const char*, 4> test_files = {
-    test_single_file,
-    test_range_file_one_header,
-    test_glob_file_header_each,
-    test_double_glob_header_each
-};
---
+++ test/basic_test.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <array>
+
+constexpr const char* test_dir_name = "http://127.0.0.1:10000/devstoreaccount1/data-test-khiops-driver-azure/khiops_data/bq_export/Adult/";
+
+constexpr const char* test_non_existent_file = "http://127.0.0.1:10000/devstoreaccount1/data-test-khiops-driver-azure/khiops_data/samples/non_existent_file.txt";
+// Actual Azure storage URI: https://myaccount.file.core.windows.net/myshare/myfolder/myfile.txt
+constexpr const char* test_single_file = "http://127.0.0.1:10000/devstoreaccount1/data-test-khiops-driver-azure/khiops_data/samples/Adult/Adult.txt";
+constexpr const char* test_glob_file = "http://127.0.0.1:10000/devstoreaccount1/data-test-khiops-driver-azure/khiops_data/bq_export/Adult/Adult-split-00000000000*.txt";
+constexpr const char* test_range_file_one_header = "http://127.0.0.1:10000/devstoreaccount1/data-test-khiops-driver-azure/khiops_data/split/Adult/Adult-split-0[0-5].txt";
+constexpr const char* test_glob_file_header_each = "http://127.0.0.1:10000/devstoreaccount1/data-test-khiops-driver-azure/khiops_data/bq_export/Adult/*.txt";
+constexpr const char* test_double_glob_header_each = "http://127.0.0.1:10000/devstoreaccount1/data-test-khiops-driver-azure/khiops_data/split/Adult_subsplit/**/Adult-split-*.txt";
+
+constexpr std::array<const char*, 4> test_files = {
+    test_single_file,
+    test_range_file_one_header,
+    test_glob_file_header_each,
+    test_double_glob_header_each
+};
