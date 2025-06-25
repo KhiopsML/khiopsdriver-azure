d0b7d85d706ab61c3c5aca12614abeb2afa8d46a
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2025-06-25 16:40:24 +0200

    Start implementing Driver class's member functions

--- src/driver.cpp
+++ src/driver.cpp
@@ -1,6 +1,52 @@
 #include "driver.h"
+#include <spdlog/spdlog.h>
 
-Driver::Driver()
+namespace az
 {
+	Driver::Driver():
+		bIsConnected(false)
+	{
+	}
 
+	string Driver::GetName() const
+	{
+		return sName;
+	}
+
+	string Driver::GetVersion() const
+	{
+		return sVersion;
+	}
+
+	string Driver::GetScheme() const
+	{
+		return sScheme;
+	}
+
+	bool Driver::IsReadOnly() const
+	{
+		return false;
+	}
+
+	size_t Driver::GetPreferredBufferSize() const
+	{
+		return sPreferredBufferSize;
+	}
+
+	void Driver::Connect()
+	{
+		spdlog::debug("Connecting");
+		bIsConnected = true;
+	}
+
+	void Driver::Disconnect()
+	{
+		spdlog::debug("Disconnecting");
+		bIsConnected = false;
+	}
+
+	bool Driver::IsConnected() const
+	{
+		return bIsConnected;
+	}
 }
