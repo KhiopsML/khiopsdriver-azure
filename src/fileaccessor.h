83b7c6302e029ae40b3c0a9b799c6def8db2e64b
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2025-06-25 16:35:11 +0200

    Define FileAccessor abstract class

--- src/fileaccessor.h
+++ src/fileaccessor.h
@@ -1,3 +1,22 @@
 #pragma once
 
-class FileAccessor;
+namespace az
+{
+	class FileAccessor;
+}
+
+#include <string>
+#include "filestream.h"
+
+namespace az
+{
+	using namespace std;
+
+	class FileAccessor
+	{
+	public:
+		virtual bool Exists() const = 0;
+		virtual size_t GetSize() const = 0;
+		virtual FileStream Open() const = 0;
+	};
+}
