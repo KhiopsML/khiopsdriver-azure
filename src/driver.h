e773da78c38e07c9252c7d1c5a0de7b6da98cb19
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2025-06-25 16:39:54 +0200

    Put driver.h declarations into 'az' namespace

--- src/driver.h
+++ src/driver.h
@@ -1,6 +1,9 @@
 #pragma once
 
-class Driver;
+namespace az
+{
+	class Driver;
+}
 
 #include <string>
 #include "fileaccessor.h"
@@ -14,31 +17,34 @@ class Driver;
 // - KHIOPS_STR(1.2.0-rc.2)
 #define DRIVER_VERSION KHIOPS_STR(0.1.0)
 
-using namespace std;
+namespace az
+{
+	using namespace std;
 
 	static const string sName = "Azure driver";
 	static const string sVersion = DRIVER_VERSION;
 	static const string sScheme = "https";
 	static const size_t sPreferredBufferSize = 4 * 1024 * 1024;
 
-class Driver
-{
-public:
-	Driver();
+	class Driver
+	{
+	public:
+		Driver();
 
-	string GetName() const;
-	string GetVersion() const;
-	string GetScheme() const;
-	bool IsReadOnly() const;
+		string GetName() const;
+		string GetVersion() const;
+		string GetScheme() const;
+		bool IsReadOnly() const;
 		size_t GetPreferredBufferSize() const;
 
-	void Connect();
-	void Disconnect();
-	bool IsConnected() const;
+		void Connect();
+		void Disconnect();
+		bool IsConnected() const;
 
 		FileAccessor CreateFileAccessor(string url);
 		FileStream RetrieveFileStream(void* handle) const;
 
-protected:
-	bool bIsConnected;
-};
+	protected:
+		bool bIsConnected;
+	};
+}
