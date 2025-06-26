14f96644a15e5b2eebf6f40022eb06d21982219e
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2025-06-26 15:05:56 +0200

    Add InvalidDomainException class

---
+++ src/exception.h
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace az
+{
+	class Exception;
+	class InvalidDomainException;
+}
+
+#include <exception>
+
+namespace az
+{
+	using namespace std;
+
+	class Exception : public exception
+	{
+	};
+
+	class InvalidDomainException : public Exception
+	{
+	};
+}
