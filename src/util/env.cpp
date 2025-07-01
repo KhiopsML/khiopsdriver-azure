a40e37e4821ae7eca9faa4ebb2c898915294df76
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2025-07-01 15:22:12 +0200

    Add GetEnvironmentVariableOrThrow

--- src/util/env.cpp
+++ src/util/env.cpp
@@ -1,28 +1,50 @@
 #include "env.hpp"
+#include <sstream>
 #include <spdlog/spdlog.h>
 #include "string.hpp"
 
 namespace az
 {
-	string GetEnvironmentVariableOrDefault(const string& varName, const string& defaultValue)
+	EnvironmentVariableNotFoundError::EnvironmentVariableNotFoundError(const string& sVarName):
+		Error("Environment variable not found"),
+		sVarName(sVarName)
 	{
-		char* value = getenv(varName.c_str());
+	}
 
-		if (value && strlen(value) > 0)
+	const string& EnvironmentVariableNotFoundError::getVarName() const
+	{
+		return sVarName;
+	}
+
+	string GetEnvironmentVariableOrThrow(const string& sVarName)
+	{
+		char* sValue = getenv(sVarName.c_str());
+		if (!sValue)
 		{
-			return value;
+			throw EnvironmentVariableNotFoundError(sVarName);
+		}
+		return sValue;
+	}
+
+	string GetEnvironmentVariableOrDefault(const string& sVarName, const string& sDefaultValue)
+	{
+		char* sValue = getenv(sVarName.c_str());
+
+		if (sValue && strlen(sValue) > 0)
+		{
+			return sValue;
 		}
 
-		string low_key = ToLower(varName);
+		string low_key = ToLower(sVarName);
 		if (low_key.find("token") || low_key.find("password") || low_key.find("key") || low_key.find("secret"))
 		{
-			spdlog::debug("No {} specified, using **REDACTED** as default.", varName);
+			spdlog::debug("No {} specified, using **REDACTED** as default.", sVarName);
 		}
 		else
 		{
-			spdlog::debug("No {} specified, using '{}' as default.", varName, defaultValue);
+			spdlog::debug("No {} specified, using '{}' as default.", sVarName, sDefaultValue);
 		}
 
-		return defaultValue;
+		return sDefaultValue;
 	}
 }
--- src/util/env.hpp
+++ src/util/env.hpp
@@ -1,10 +1,27 @@
 #pragma once
 
+namespace az
+{
+	class EnvironmentVariableNotFoundError;
+}
+
 #include <string>
+#include "../exception.hpp"
 
 using namespace std;
 
 namespace az
 {
-	string GetEnvironmentVariableOrDefault(const string& variable_name, const string& default_value);
+	class EnvironmentVariableNotFoundError : Error
+	{
+	public:
+		EnvironmentVariableNotFoundError(const string& sVarName);
+		const string& getVarName() const;
+
+	private:
+		string sVarName;
+	};
+
+	string GetEnvironmentVariableOrThrow(const string& sVarName);
+	string GetEnvironmentVariableOrDefault(const string& sVarName, const string& sDefaultValue);
 }
