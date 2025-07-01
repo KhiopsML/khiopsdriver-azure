906872a8b076881cd7262a7fb23219d6242682c5
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2025-07-01 15:58:59 +0200

    Fix connection string parsing

--- src/util/connstring.cpp
+++ src/util/connstring.cpp
@@ -12,16 +12,16 @@ namespace az
 	{
 		smatch match;
 		Account account;
-		if (!regex_search(connectionString, match, regex("AccountName=[^;]+")))
+		if (!regex_search(connectionString, match, regex("AccountName=([^;]+)")))
 		{
 			throw ParsingError("Failed to parse connection string: account name not found");
 		}
-		account.name = match[0];
-		if (!regex_search(connectionString, match, regex("AccountKey=[^;]+")))
+		account.name = match[1];
+		if (!regex_search(connectionString, match, regex("AccountKey=([^;]+)")))
 		{
 			throw ParsingError("Failed to parse connection string: account key not found");
 		}
-		account.key = match[0];
+		account.key = match[1];
 		return account;
 	}
 }
