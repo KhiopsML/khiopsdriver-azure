1e769c68083016dd6b150baedb240e7adfef94e3
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2025-07-21 16:06:35 +0200

    Add az::globbing::RegexFromGlobbingPattern

--- src/util/glob.cpp
+++ src/util/glob.cpp
@@ -1,5 +1,4 @@
 #include "glob.hpp"
-#include <regex>
 
 namespace az
 {
@@ -9,6 +8,17 @@ namespace globbing
 	{
 		smatch match;
 		return regex_search(str, regex("[^\\][*?![^]")) ? match.position() : string::npos;
+	}
+
+	regex RegexFromGlobbingPattern(const string& sGlobbingPattern)
+	{
+		return regex(
+			regex_replace(
+				regex_replace(sGlobbingPattern, regex("?"), "."),
+				regex("*"),
+				".*"
+			)
+		);
 	}
 }
 }
--- src/util/glob.hpp
+++ src/util/glob.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <regex>
 
 namespace az
 {
@@ -8,5 +9,6 @@ using namespace std;
 namespace globbing
 {
 	size_t FindGlobbingChar(const string& str);
+	regex RegexFromGlobbingPattern(const string& sGlobbingPattern);
 }
 }
