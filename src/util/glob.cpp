2262d7527aab883017f8454762684b15bd6fd81f
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2025-07-25 16:11:50 +0200

    Remove unused function

--- src/util/glob.cpp
+++ src/util/glob.cpp
@@ -9,24 +9,5 @@ namespace globbing
 		smatch match;
 		return regex_search(str, regex("[^\\][*?![^]")) ? match.position() : string::npos;
 	}
-
-	// TODO: Maybe this function should escape more things like pluses chars and the like
-	regex RegexFromGlobbingPattern(const string& sGlobbingPattern)
-	{
-		return regex(
-			regex_replace(
-				regex_replace(
-					regex_replace(
-						sGlobbingPattern,
-						regex("\\."),
-						"\\."
-					),
-					regex("?"),
-					"."),
-				regex("*"),
-				".*"
-			)
-		);
-	}
 }
 }
