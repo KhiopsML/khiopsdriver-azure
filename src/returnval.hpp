691ed7db94f411bd1ed30bc0320cae0fe801c56a
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2025-07-28 15:21:11 +0200

    Fix type of constant nSizeFailure

--- src/returnval.hpp
+++ src/returnval.hpp
@@ -6,7 +6,7 @@ namespace az
 	const int nFailure = 0;
 	const int nTrue = 1;
 	const int nFalse = 0;
-	const int nSizeFailure = -1;
+	const long long int nSizeFailure = -1;
 	const int nCloseSuccess = 0;
 	const int nCloseFailure = -1;
 	const int nReadFailure = -1;
