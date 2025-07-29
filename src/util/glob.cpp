09e4bf8c7ea47b0131cdb6f275e1fe31061f6c12
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2025-07-29 18:04:18 +0200

    Fix az::globbing::FindGlobbingChar

--- src/util/glob.cpp
+++ src/util/glob.cpp
@@ -9,7 +9,7 @@ namespace globbing
 	size_t FindGlobbingChar(const string& str)
 	{
 		smatch match;
-		return regex_search(str, regex("[^\\][*?![^]")) ? match.position() : string::npos;
+		return regex_search(str, match, regex("[^\\]([*?![^])", regex_constants::extended)) ? match.position(1) : string::npos;
 	}
 }
 }
