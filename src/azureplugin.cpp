c49639f0c8f1bd70c1a584051f5d6c731eccea5a
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2025-07-30 11:22:44 +0200

    Fix compilation under MSVC

--- src/azureplugin.cpp
+++ src/azureplugin.cpp
@@ -41,7 +41,7 @@
 using namespace std;
 using namespace az;
 
-constexpr char* emulated_storage_connection_string =
+constexpr char emulated_storage_connection_string[] =
 	"DefaultEndpointsProtocol=http;"
 	"AccountName=devstoreaccount1;"
 	"AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;"
