329d66d07c7d831fdae3ee92560af17f16df8a0e
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2025-09-05 16:42:56 +0200

    Remove Blob+Share Write/Append internal loop

--- src/blobappender.cpp
+++ src/blobappender.cpp
@@ -33,23 +33,20 @@ namespace az
 		catch (const Azure::Storage::StorageException& exc)
 		{
 		}
+
+		size_t nToWrite = nSize * nCount;
+
 		vector<string> blockIds;
 		transform(blocks.begin(), blocks.end(), back_inserter(blockIds), [](const auto& block) { return block.Name; });
-		size_t nToWrite = nSize * nCount;
-		size_t nWritten = 0;
-		while (nToWrite > 0)
-		{
-			string sBlockIdInBase10 = (ostringstream() << setfill('0') << setw(64) << blockIds.size()).str();
-			vector<uint8_t> blockIdInBase10(sBlockIdInBase10.begin(), sBlockIdInBase10.end());
-			string sBlockIdInBase64 = Azure::Core::Convert::Base64Encode(blockIdInBase10);
-			size_t n = nToWrite > 100 * 1024 * 1024 ? 100 * 1024 * 1024 : nToWrite;
-			Azure::Core::IO::MemoryBodyStream bodyStream(((const uint8_t*)source) + nWritten, n);
-			nToWrite -= n;
-			nWritten += n;
-			client.StageBlock(sBlockIdInBase64, bodyStream);
-			blockIds.push_back(sBlockIdInBase64);
-		}
+		string sBlockIdInBase10 = (ostringstream() << setfill('0') << setw(64) << blockIds.size()).str();
+		vector<uint8_t> blockIdInBase10(sBlockIdInBase10.begin(), sBlockIdInBase10.end());
+		string sBlockIdInBase64 = Azure::Core::Convert::Base64Encode(blockIdInBase10);
+
+		Azure::Core::IO::MemoryBodyStream bodyStream((const uint8_t*)source, nToWrite);
+		client.StageBlock(sBlockIdInBase64, bodyStream);
+		blockIds.push_back(sBlockIdInBase64);
 		client.CommitBlockList(blockIds);
-		return nWritten;
+
+		return nToWrite;
 	}
 }
--- src/sharewriter.cpp
+++ src/sharewriter.cpp
@@ -27,19 +27,15 @@ namespace az
 		Azure::Storage::Files::Shares::Models::FileHttpHeaders httpHeaders;
 		Azure::Storage::Files::Shares::Models::FileSmbProperties smbProperties;
 		Azure::Storage::Files::Shares::SetFilePropertiesOptions opts;
+
 		size_t nToWrite = nSize * nCount;
-		size_t nWritten = 0;
-		while (nToWrite > 0)
-		{
-			size_t n = nToWrite > 100 * 1024 * 1024 ? 100 * 1024 * 1024 : nToWrite;
-			Azure::Core::IO::MemoryBodyStream bodyStream(((const uint8_t*)source) + nWritten, n);
-			nToWrite -= n;
-			nWritten += n;
-			opts.Size = nCurrentPos + n;
-			client.SetProperties(httpHeaders, smbProperties, opts);
-			client.UploadRange(nCurrentPos, bodyStream);
-			nCurrentPos += n;
-		}
-		return nWritten;
+
+		Azure::Core::IO::MemoryBodyStream bodyStream((const uint8_t*)source, nToWrite);
+		opts.Size = nCurrentPos + nToWrite;
+		client.SetProperties(httpHeaders, smbProperties, opts);
+		client.UploadRange(nCurrentPos, bodyStream);
+		nCurrentPos += nToWrite;
+
+		return nToWrite;
 	}
 }
