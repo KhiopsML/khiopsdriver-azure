22d258abf2089576f287ee32d54010191ea9e426
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2025-09-24 16:03:57 +0200

    Change concurrent access test to check that a new reading following a re-opening of the file works
    When you get a read failure, this may be due to an update to the file you are trying to read. You can check it with driver_getlasterror. Then you can try to read the file again and it should work (unless the same error case is happening).

--- test/io_test.cpp
+++ test/io_test.cpp
@@ -64,31 +64,44 @@ void TestFSeek(string sUrl, bool bCrLf)
 	delete[] buffer;
 }
 
-TEST_P(IoTest, FReadMultipartFileWithConcurrentWrite)
+TEST_P(IoTest, FReadWithConcurrentWrite)
 {
-	string sOutputFile = url.RandomOutputFile();
+	string file = url.RandomOutputFile();
+	char ibuffer[64] {};
 	void* ihandle;
 	void* ohandle;
-	const char* oBuffer = "abc";
-	char iBuffer[2] = { 0, 0 }; // We will read bytes one by one and the terminating NULL byte will already be there for string comparisons
+
 	ASSERT_EQ(driver_connect(), nSuccess);
-	ASSERT_NE(ohandle = driver_fopen(sOutputFile.c_str(), 'w'), nullptr);
+
 	// Write initial data to the file
-	ASSERT_EQ(driver_fwrite(oBuffer, 1, strlen(oBuffer), ohandle), strlen(oBuffer));
+	ASSERT_NE(ohandle = driver_fopen(file.c_str(), 'w'), nullptr);
+	ASSERT_EQ(driver_fwrite("abc", 1, 3, ohandle), 3);
 	ASSERT_EQ(driver_fflush(ohandle), nFlushSuccess);
-	// Open the file for rading. Internally this will fetch the ETag of the file
-	ASSERT_NE(ihandle = driver_fopen(sOutputFile.c_str(), 'r'), nullptr);
+
+	// Open the file for reading. Internally this will fetch the ETag of the file
+	ASSERT_NE(ihandle = driver_fopen(file.c_str(), 'r'), nullptr);
 	// This first reading operation should find an ETag identical to the one fetched previously
-	ASSERT_EQ(driver_fread(iBuffer, 1, 1, ihandle), 1);
-	ASSERT_STREQ(iBuffer, "a");
-	// Modify the file to change its ETag
-	oBuffer = "def";
-	ASSERT_EQ(driver_fwrite(oBuffer, 1, strlen(oBuffer), ohandle), strlen(oBuffer));
+	ASSERT_EQ(driver_fread(ibuffer, 1, 3, ihandle), 3);
+	ASSERT_STREQ(ibuffer, "abc");
+
+	// Add some data to the file. The ETag of the file will be changed
+	ASSERT_EQ(driver_fwrite("def", 1, 3, ohandle), 3);
 	ASSERT_EQ(driver_fflush(ohandle), nFlushSuccess);
+
 	// This second reading operation should fail because it should find an ETag different to the one fetched by the driver_fopen call
-	ASSERT_EQ(driver_fread(iBuffer, 1, 1, ihandle), nReadFailure);
-	ASSERT_EQ(driver_fclose(ohandle), nCloseSuccess);
+	ASSERT_EQ(driver_fread(ibuffer, 1, 6, ihandle), nReadFailure);
+	ASSERT_STREQ(ibuffer, "abc"); // Input buffer content unchanged
+
+	// Open file again. This will fetch the new ETag
 	ASSERT_EQ(driver_fclose(ihandle), nCloseSuccess);
-	ASSERT_EQ(driver_remove(sOutputFile.c_str()), nSuccess);
+	ASSERT_NE(ihandle = driver_fopen(file.c_str(), 'r'), nullptr);
+	// Now read the content again. It should be the new one
+	ASSERT_EQ(driver_fread(ibuffer, 1, 6, ihandle), 6);
+	ASSERT_STREQ(ibuffer, "abcdef");
+
+	ASSERT_EQ(driver_fclose(ihandle), nCloseSuccess);
+	ASSERT_EQ(driver_fclose(ohandle), nCloseSuccess);
+	ASSERT_EQ(driver_remove(file.c_str()), nSuccess);
+
 	ASSERT_EQ(driver_disconnect(), nSuccess);
 }
