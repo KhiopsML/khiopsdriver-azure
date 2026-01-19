c541bfe72f013838f6a221f712905f50a792b55b
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2026-01-19 16:00:23 +0100

    Format code

--- test/internal/connstring_test.cpp
+++ test/internal/connstring_test.cpp
@@ -56,8 +56,11 @@ TEST(ConnectionStringTest, ParseValidConnString) {
   ASSERT_EQ(actual, expected);
 }
 
-const char *sValidAzuriteConnString = 
-    "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://localhost:10000/devstoreaccount1;QueueEndpoint=http://localhost:10001/devstoreaccount1;";
+const char *sValidAzuriteConnString =
+    "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey="
+    "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/"
+    "KBHBeksoGMGw==;BlobEndpoint=http://localhost:10000/"
+    "devstoreaccount1;QueueEndpoint=http://localhost:10001/devstoreaccount1;";
 
 TEST(ConnectionStringTest, ParseAzuriteValidConnString) {
   az::util::connstr::ConnectionString actual =
