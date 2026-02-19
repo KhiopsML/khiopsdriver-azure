cf47827a4905294e4c29ca05f8e62816d5f2ea6f
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2026-02-19 18:40:21 +0100

    Merge some CPP files in HPP files

--- src/objectclient.hpp
+++ src/objectclient.hpp
@@ -14,10 +14,33 @@ struct ObjectClient {
     Azure::Storage::Blobs::BlobClient blob;
     Azure::Storage::Files::Shares::ShareFileClient shareFile;
   };
-  ObjectClient(const ObjectClient &source);
-  ObjectClient(const Azure::Storage::Blobs::BlobClient &client);
-  ObjectClient(const Azure::Storage::Files::Shares::ShareFileClient &client);
-  ~ObjectClient();
-  ObjectClient &operator=(const ObjectClient &source);
+  ObjectClient(const ObjectClient &source) : tag(source.tag) {
+    if (tag == BLOB)
+      new (&blob) Azure::Storage::Blobs::BlobClient(source.blob);
+    else
+      new (&shareFile)
+          Azure::Storage::Files::Shares::ShareFileClient(source.shareFile);
+  }
+  ObjectClient(const Azure::Storage::Blobs::BlobClient &client) : tag(BLOB) {
+    new (&blob) Azure::Storage::Blobs::BlobClient(client);
+  }
+  ObjectClient(const Azure::Storage::Files::Shares::ShareFileClient &client)
+      : tag(SHARE) {
+    new (&shareFile) Azure::Storage::Files::Shares::ShareFileClient(client);
+  }
+  ~ObjectClient() {
+    if (tag == BLOB)
+      blob.~BlobClient();
+    else
+      shareFile.~ShareFileClient();
+  }
+  ObjectClient &operator=(const ObjectClient &source) {
+    tag = source.tag;
+    if (tag == BLOB)
+      blob = source.blob;
+    else
+      shareFile = source.shareFile;
+    return *this;
+  }
 };
 } // namespace az
