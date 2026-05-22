91f22d40c7ed95b216021d93b155c6d4dc91177e
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2026-05-22 09:43:15 +0200

    Refactor

--- src/khiops_driver_azure/backend.cpp
+++ src/khiops_driver_azure/backend.cpp
@@ -6,7 +6,11 @@
 
 using namespace std;
 
-namespace khiops_driver_azure {
+namespace khiops_driver_common {
+
+spdlog::logger *GetLogger() {
+    return GetLogger("azdriver", "AZURE_DRIVER_LOGFILE", "AZURE_DRIVER_LOGLEVEL");
+}
 
 int GetDriverName(std::string *result) {
     *result = "Azure driver";
@@ -23,24 +27,109 @@ int GetDriverScheme(std::string *result) {
     return 0;
 }
 
-spdlog::logger *GetLogger() {
-    return khiops_driver_common::GetLogger("azdriver", "AZURE_DRIVER_LOGFILE", "AZURE_DRIVER_LOGLEVEL");
+int IsReadOnly(bool *result) {
+    GetLogger()->error("Not implemented!");
+    return -1;
 }
 
+int Initialize() {
+    GetLogger()->error("Not implemented!");
+    return -1;
 }
 
-namespace khiops_driver_common {
+int Finalize() {
+    GetLogger()->error("Not implemented!");
+    return -1;
+}
 
-const Backend *GetBackend() {
-    static unique_ptr<Backend> backend = nullptr;
-    if (backend == nullptr) {
-        backend = make_unique<Backend>();
-        backend->GetDriverName = &khiops_driver_azure::GetDriverName;
-        backend->GetLogger = &khiops_driver_azure::GetLogger;
-        backend->GetDriverVersion = &khiops_driver_azure::GetDriverVersion;
-        backend->GetDriverScheme = &khiops_driver_azure::GetDriverScheme;
-    }
-    return backend.get();
+int GetSystemPreferredBufferSize(size_t *result) {
+    GetLogger()->error("Not implemented!");
+    return -1;
+}
+
+int FileExists(bool *result, const std::string &sFilePathName) {
+    GetLogger()->error("Not implemented!");
+    return -1;
+}
+
+int DirExists(bool *result, const std::string &sFilePathName) {
+    GetLogger()->error("Not implemented!");
+    return -1;
+}
+
+int GetFileSize(size_t *result, const std::string &filename) {
+    GetLogger()->error("Not implemented!");
+    return -1;
+}
+
+int FOpen(khiops_driver_common::FileStream &stream, const std::string &filename) {
+    GetLogger()->error("Not implemented!");
+    return -1;
+}
+
+int FClose(const khiops_driver_common::FileStream &stream) {
+    GetLogger()->error("Not implemented!");
+    return -1;
+}
+
+int FRead(size_t *result, void *ptr, size_t size, size_t count, khiops_driver_common::FileStream &stream) {
+    GetLogger()->error("Not implemented!");
+    return -1;
+}
+
+int FSeek(khiops_driver_common::FileStream &stream, long long int offset, int whence) {
+    GetLogger()->error("Not implemented!");
+    return -1;
+}
+
+int FWrite(size_t *result, const void *ptr, size_t size, size_t count, const khiops_driver_common::FileStream &stream) {
+    GetLogger()->error("Not implemented!");
+    return -1;
+}
+
+int FFlush(const khiops_driver_common::FileStream &stream) {
+    GetLogger()->error("Not implemented!");
+    return -1;
+}
+
+int Remove(const std::string &filename) {
+    GetLogger()->error("Not implemented!");
+    return -1;
+}
+
+int Mkdir(const std::string &pathname) {
+    GetLogger()->error("Not implemented!");
+    return -1;
+}
+
+int Rmdir(const std::string &pathname) {
+    GetLogger()->error("Not implemented!");
+    return -1;
+}
+
+int DiskFreeSpace(size_t *result, const std::string &filename) {
+    GetLogger()->error("Not implemented!");
+    return -1;
+}
+
+int CopyToLocal(const std::string &sourcefilename, const std::string &destfilename) {
+    GetLogger()->error("Not implemented!");
+    return -1;
+}
+
+int CopyFromLocal(const std::string &sourcefilename, const std::string &destfilename) {
+    GetLogger()->error("Not implemented!");
+    return -1;
+}
+
+int Concat(const std::string &destfilename, const std::vector<std::string> &sourcefilenames) {
+    GetLogger()->error("Not implemented!");
+    return -1;
+}
+
+int ComposeMultifile(const std::string &sDestFilePathName, const std::vector<std::string> &sSourceFilePathNames) {
+    GetLogger()->error("Not implemented!");
+    return -1;
 }
 
 }
